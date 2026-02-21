d55cf69dee4957fe53d5d6fe54700a3e7bdb9469
Ted Son <tedson@theedsons.com>
2026-02-21 15:45:47 -0600

    Standardized all pages to use data-include attributes and absolute paths for script-based loading.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Alice is online.');
+$(function () {
+    $('[data-include]').each(function () {
+        var file = '/' + $(this).data('include') + '.html';
+        $(this).load(file);
+    });
+    console.log('Alice Systems: Navigation and Header loaded.');
+});
