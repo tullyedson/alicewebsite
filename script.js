52478cb3c6beebbf1c7ea566b9e02d73c4961553
Ted Son <tedson@theedsons.com>
2026-02-21 17:29:40 -0600

    Updated gallery page with dynamic header/footer system

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-$(function () {
-    $('[data-include]').each(function () {
-        var file = '/' + $(this).data('include') + '.html';
-        $(this).load(file);
-    });
-});
+console.log('Alice is online.');
